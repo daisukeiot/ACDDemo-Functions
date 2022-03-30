f33c4a4f2d5897a1201eaf61d4a398397ec03a22
Daisuke Nakahara <daisuken@msn.com>
2022-03-29 21:22:35 -0700

    Initial Release

---
+++ ACSDemo-Functions/GetToken.cs
@@ -0,0 +1,98 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Azure.Communication.Identity;
+using Azure;
+using Azure.Communication;
+using System.Net;
+
+namespace Microsoft.ACDDEmo
+{
+    public static class GetToken
+    {
+        [FunctionName("GetToken")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+            CommunicationIdentityClient client;
+            Response<CommunicationUserIdentifierAndToken> identityAndTokenResponse;
+            CommunicationUserIdentifier identity;
+            String token;
+            DateTimeOffset expiresOn;
+            IActionResult requestResponse;
+
+            try
+            {
+                client = new CommunicationIdentityClient(connectionString);
+
+            }
+            catch (InvalidOperationException ex)
+            {
+                log.LogError($"CommunicationIdentityClient() initilization failed. : Exception {ex.GetType()} : {ex.Message}");
+                requestResponse = CreateResponse(ex.Message, HttpStatusCode.BadRequest);
+                return new BadRequestObjectResult(requestResponse);
+            }
+
+            try
+            {
+                identityAndTokenResponse = await client.CreateUserAndTokenAsync(scopes: new[] { CommunicationTokenScope.VoIP });
+                var response = identityAndTokenResponse.GetRawResponse();
+
+                if (response.Status != 201)
+                {
+                    log.LogError($"CreateUserAndTokenAsync() returned status {response.Status}");
+                    requestResponse = CreateResponse($"CreateUserAndTokenAsync() returned status {response.Status}", HttpStatusCode.InternalServerError);
+                    return new BadRequestObjectResult(requestResponse);
+                }
+
+                // Retrieve the identity, token, and expiration date from the response
+                identity = identityAndTokenResponse.Value.User;
+                token = identityAndTokenResponse.Value.AccessToken.Token;
+                expiresOn = identityAndTokenResponse.Value.AccessToken.ExpiresOn;
+
+                var responseContent = new
+                {
+                    userId = identity.Id,
+                    token = token,
+                    expiresOn = expiresOn
+                };
+
+                requestResponse = CreateResponse(responseContent, HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                log.LogError($"CreateUserAndTokenAsync() failed : Exception {ex.GetType()} : {ex.Message}");
+                requestResponse = CreateResponse(ex.Message, HttpStatusCode.InternalServerError);
+                return new BadRequestObjectResult(requestResponse);
+            }
+
+            // Print the details to the screen
+            log.LogInformation($"{identityAndTokenResponse.Value.AccessToken.ToString()}");
+            log.LogInformation($"\nCreated an identity with ID: {identity.Id}");
+            log.LogInformation($"\nIssued an access token with 'voip' scope that expires at {expiresOn}:");
+            log.LogInformation(token);
+
+            //Console.WriteLine($"\nCreated an identity with ID: {identity.Id}");
+            string responseMessage = $"{{IdentityId : \"{identity}\", token : \"{token}\"}}";
+
+            return new OkObjectResult(requestResponse);
+        }
+
+        private static IActionResult CreateResponse(object message, HttpStatusCode httpCode)
+        {
+            ContentResult result = new ContentResult();
+            result.Content = JsonConvert.SerializeObject(message, Formatting.None);
+            result.ContentType = "application/json";
+            result.StatusCode = (int)httpCode;
+            return result;
+        }
+    }
+}
---
+++ ACSDemo-Functions/RefreshToken.cs
@@ -0,0 +1,91 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Azure.Communication.Identity;
+using Azure.Core;
+using Azure;
+using System.Net;
+using Azure.Communication;
+
+namespace ACSDemo_Functions
+{
+    public static class RefreshToken
+    {
+        [FunctionName("RefreshToken")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+            CommunicationIdentityClient client;
+            IActionResult requestResponse;
+            Response<AccessToken> tokenResponse;
+
+            try
+            {
+                client = new CommunicationIdentityClient(connectionString);
+
+            }
+            catch (InvalidOperationException ex)
+            {
+                log.LogError($"CommunicationIdentityClient() initilization failed. : Exception {ex.GetType()} : {ex.Message}");
+                requestResponse = CreateResponse(ex.Message, HttpStatusCode.BadRequest);
+                return new BadRequestObjectResult(requestResponse);
+            }
+
+            string acsId = req.Query["id"];
+
+            try
+            {
+                var identityToRefresh = new CommunicationUserIdentifier(acsId);
+                tokenResponse = await client.GetTokenAsync(identityToRefresh, scopes: new[] { CommunicationTokenScope.VoIP });
+
+                var response = tokenResponse.GetRawResponse();
+
+                if (response.Status != 200)
+                {
+                    log.LogError($"GetTokenAsync() returned status {response.Status}");
+                    requestResponse = CreateResponse($"GetTokenAsync() returned status {response.Status}", HttpStatusCode.InternalServerError);
+                    return new BadRequestObjectResult(requestResponse);
+                }
+
+                var responseContent = new
+                {
+                    token = tokenResponse.Value.Token,
+                    expiresOn = tokenResponse.Value.ExpiresOn
+                };
+
+                requestResponse = CreateResponse(responseContent, HttpStatusCode.OK);
+
+            }
+            catch (Exception ex)
+            {
+                log.LogError($"CreateUserAndTokenAsync() failed : Exception {ex.GetType()} : {ex.Message}");
+                requestResponse = CreateResponse(ex.Message, HttpStatusCode.InternalServerError);
+                return new BadRequestObjectResult(requestResponse);
+            }
+
+            log.LogInformation($"{tokenResponse.Value.Token.ToString()}");
+            log.LogInformation($"\nCreated an identity with ID: {acsId}");
+            log.LogInformation($"\nIssued an access token with 'voip' scope that expires at {tokenResponse.Value.ExpiresOn}:");
+
+
+            return new OkObjectResult(requestResponse);
+        }
+
+        private static IActionResult CreateResponse(object message, HttpStatusCode httpCode)
+        {
+            ContentResult result = new ContentResult();
+            result.Content = JsonConvert.SerializeObject(message, Formatting.None);
+            result.ContentType = "application/json";
+            result.StatusCode = (int)httpCode;
+            return result;
+        }
+    }
+}
